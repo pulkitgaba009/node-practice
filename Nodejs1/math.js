cf0ab6e2863a65bcf9e9dc0b6ed4ab372c1ecb79
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-07-25 20:07:44 +0530

    node js

---
+++ Nodejs1/math.js
@@ -0,0 +1,11 @@
+let pi = 3.145
+
+const sum = (a,b) =>{
+    return a+b;
+}
+
+const mul = (a,b) =>{
+    return a+b;
+}
+
+module.exports =pi ;
---
+++ Nodejs1/script.js
@@ -0,0 +1,16 @@
+// let n = 5 
+
+// for(let i = 0 ; i< n ;i++){
+//     console.log(i)
+// }
+
+// // using argv
+// let args= process.argv;
+
+// for(let i = 0 ;i<args.length ;i++ ){
+//     console.log(`hi ${args[i]}`)
+// }
+
+let pa = require("./math");
+
+console.log(pa)
