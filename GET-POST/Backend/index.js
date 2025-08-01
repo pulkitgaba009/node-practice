ba913ec516779d66df5399bd152e1294ab79b6fb
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-08-01 22:36:11 +0530

    worked on post and get req

---
+++ GET-POST/Backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 3000;
+
+// Middleware to use post request
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/index.html")); // <-- FIXED
+});
+
+app.get("/register",(req,res)=>{
+    const {username,password} = req.query;
+    console.log(`${username} - ${password}`)
+    res.send("Standard GET response...");
+})
+
+app.post("/register",(req,res)=>{
+    console.log(req.body);
+    res.send("Standard POST response...");
+})
+
+app.listen(port,()=>{
+    console.log("app is working ...");
+})
