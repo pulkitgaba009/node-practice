c2205bd62d2af5b4b8eeabd97b6599b531df99f8
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-08-06 19:36:20 +0530

    Post api

--- REST/index.js
+++ REST/index.js
@@ -35,6 +35,19 @@ app.get("/posts",(req,res)=>{
     res.render("index.ejs",{posts})
 })
 
+// first created get request to get the form
+app.get("/posts/new",(req,res)=>{
+    res.render("newPost.ejs");
+})
+
+// Created post API to
+app.post("/posts",(req,res)=>{
+    let {userName,content} = req.body;
+    console.log(userName,content)
+    posts.push({userName,content});
+    res.redirect("/posts");
+})
+
 
 
 app.listen(port,()=>{
